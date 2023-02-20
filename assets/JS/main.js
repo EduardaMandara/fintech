2506cc28bb7100423457a195885aaf4d65642e1e
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-02-20 08:23:32 -0300

    testando alert da pagina.

---
+++ assets/JS/main.js
@@ -0,0 +1,8 @@
+import { Toast } from 'bootstrap.esm.min.js'
+
+Array.from(document.querySelectorAll('.toast'))
+.forEach(toastNode => new Toast(toastNode));
+
+function alert() {
+    alert("ATENÇÂO!")
+} ;
