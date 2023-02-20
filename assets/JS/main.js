b6fa9a7867d3e30cfe4375812a28a6424bbaf5be
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-02-20 08:26:13 -0300

    .

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,6 +3,6 @@ import { Toast } from 'bootstrap.esm.min.js'
 Array.from(document.querySelectorAll('.toast'))
 .forEach(toastNode => new Toast(toastNode));
 
-function alert() {
+function primeiroAcesso() {
     alert("ATENÇÂO!")
 } ;
